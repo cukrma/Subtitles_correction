3fc6b291c1ee8056b4c5cbeed0ef89b3c03cd75c
cukrma <90902627+cukrma@users.noreply.github.com>
2021-09-27 19:30:16 +0200

    Add files via upload

--- Source.py
+++ Source.py
@@ -4,8 +4,14 @@ import codecs
 print("Program pro opravu českých titulků.\n")
 print("Níže zadejte cestu a název souboru bez přípony. Pokud je soubor ve stejné složce, stačí pouze název souboru.\n")
 file_name = input("Zadejte cestu a název souboru: ")
+print("\n\n1) Titulkový soubor (.srt)\n2) Textový soubor (.txt)\n")
+file_type = input("Typ souboru (uveďte číslo): ")
 
-file = codecs.open(file_name + ".srt", "r", "ANSI")
+# rozliseni typu souboru
+if file_type == 1:
+    file = codecs.open(file_name + ".srt", "r", "ANSI")
+else:
+    file = codecs.open(file_name + ".txt", "r", "ANSI")
 text = file.read()
 file.close()
 
@@ -17,6 +23,9 @@ my_dict = [ ["ì", "ě"], ["Ì", "Ě"], ["ù", "ů"], ["Ù", "Ů"], ["è", "č"]
 for i in range(len(my_dict)):
     text = text.replace(my_dict[i][0], my_dict[i][1])
 
-file = codecs.open(file_name + "-fixed.srt", "w", "utf-8")
+if file_type == 1:
+    file = codecs.open(file_name + "-fixed.srt", "w", "utf-8")
+else:
+    file = codecs.open(file_name + "-fixed.txt", "w", "utf-8")
 file.write(text)
 file.close()
